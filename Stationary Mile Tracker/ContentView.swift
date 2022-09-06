5a6704ee08c41c2f106052752c835601b6904d82
Erica Stevens <ericastevens99@gmail.com>
2022-09-06 01:50:17 -0400

    Initial Commit

---
+++ Stationary Mile Tracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Stationary Mile Tracker
+//
+//  Created by Erica Stevens on 9/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stationary Mile Tracker/Stationary_Mile_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stationary_Mile_TrackerApp.swift
+//  Stationary Mile Tracker
+//
+//  Created by Erica Stevens on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Stationary_Mile_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
