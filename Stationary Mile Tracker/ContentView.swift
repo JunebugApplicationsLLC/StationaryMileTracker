007c7d6ae9dd4157aad3bc281b6c95c734bf736f
Erica Stevens <ericastevens99@gmail.com>
2022-09-08 01:40:53 -0400

    Use Chart instead of list

--- Stationary Mile Tracker/ContentView.swift
+++ Stationary Mile Tracker/ContentView.swift
@@ -7,19 +7,31 @@
 
 import CalendarProgressTracker
 import SwiftUI
+import Charts
 
 struct ContentView: View {
     @Environment(\.calendar) var calendar: Calendar
     @Environment(\.timeZone) var timeZone: TimeZone
     @ObservedObject var trackedMileageViewModel = TrackedMileageViewModel()
+    @State var goal: Int = 50
+    var totalMilesTracked: Int {
+        var trackedMiles = 0
+        trackedMileageViewModel.milesTrackedForDay.forEach { day, miles in
+            trackedMiles += miles
+        }
+        return trackedMiles
+    }
     var body: some View {
         VStack {
             CalendarTrackerView(viewModel: trackedMileageViewModel)
-            List {
-                ForEach(Array(trackedMileageViewModel.milesTrackedForDay.keys), id: \.self) { key in
-                    Text("\(key.monthName) \(key.name) \(key.date): \(trackedMileageViewModel.milesTrackedForDay[key] ?? 0) miles")
-                }
+            Chart(Array(trackedMileageViewModel.milesTrackedForDay.keys)) { key in
+                LineMark(
+                    x: PlottableValue.value("Date", "\(key.name) \(key.date)") ,
+                    y: PlottableValue.value("Miles", trackedMileageViewModel.milesTrackedForDay[key] ?? 0)
+                )
             }
+            .padding()
+            Text("Total Miles: \(totalMilesTracked)")
         }
         
     }
