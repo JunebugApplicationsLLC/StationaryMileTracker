0749a3ae9d55d36da4ecabc3cba23bcc5af2a9e4
Erica Stevens <ericastevens99@gmail.com>
2022-09-10 04:01:52 -0400

    Interface updates

--- Stationary Mile Tracker/ContentView.swift
+++ Stationary Mile Tracker/ContentView.swift
@@ -35,18 +35,50 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            CalendarTrackerView(viewModel: trackedMileageViewModel)
-            Chart(trackedDays) { day in
-                LineMark(
-                    x: PlottableValue.value("Date", "\(day.name) \(day.date)") ,
-                    y: PlottableValue.value("Miles", trackedMileageViewModel.milesTrackedForDay[day] ?? 0)
-                )
+        NavigationView {
+            GeometryReader { proxy in
+                VStack {
+                    CalendarTrackerView(viewModel: trackedMileageViewModel)
+                    milesTrendChart
+                        .frame(height: proxy.size.height * 0.4)
+                }
             }
-            .padding()
+            .navigationTitle("Monthly Mile Tracker")
+        }
+    }
+    
+    var goalButton: some View {
+        Text("Goal: 500")
+            .padding(8)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8.0)
+    }
+    
+    var milesTrendChart: some View {
+        VStack {
+           goalButton
+            if trackedDays.count < 2 {
+                Spacer()
+                Text("Log more miles to see trends over time")
+                    .font(.caption).italic()
+                Spacer()
+            } else {
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Miles Per Tracked Day")
+                        .font(.subheadline)
+                    Chart(trackedDays) { day in
+                        LineMark(
+                            x: PlottableValue.value("Date", "\(day.name) \(day.date)") ,
+                            y: PlottableValue.value("Miles", trackedMileageViewModel.milesTrackedForDay[day] ?? 0)
+                        )
+                        .foregroundStyle(.pink)
+                    }
+                }
+                .padding()
+            }
             Text("Total Miles: \(totalMilesTracked)")
         }
-        
     }
 }
 
